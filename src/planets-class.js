3913a0793baa26408a8765d6bc496176e7a1f74d
Christopher Huber <CHuber42.work@gmail.com>
2020-04-17 08:40:26 -0700

    add constructor for planets; next: test

---
+++ src/planets-class.js
@@ -0,0 +1,32 @@
+export class Planet {
+  constructor (name) {
+    this.name = name;
+    
+    switch (name) {
+      case "Mercury":
+        this.ratio = (365/88);
+        break;
+      case "Venus":
+        this.ratio = (365/224.7);
+        break; 
+      case "Earth":
+        this.ratio = 1;
+        break;
+      case "Mars":
+        this.ratio = (365/687);
+        break;
+      case "Jupiter":
+        this.ratio = (365/4333);
+        break;
+      case "Saturn":
+        this.ratio = (365/10755);
+        break;  
+      case "Uranus":
+        this.ratio = (365/30687);
+        break;
+      case "Neptune":
+        this.ratio = (365/60190);
+        break;
+    }
+  }
+}
